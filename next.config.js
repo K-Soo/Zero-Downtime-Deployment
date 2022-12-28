2d5dbf821392cb1baa8e3227cdbc8e934103f455
K-Soo <kanabun102@naver.com>
2022-12-28 16:01:19 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+const nextBuildId = require("next-build-id");
 const nextConfig = {
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return "my-build-id";
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
