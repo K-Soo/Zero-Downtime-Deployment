ce936611f2f1ff922935d555cea0d10c85437fea
K-Soo <kanabun102@naver.com>
2022-12-28 12:04:24 +0900

    add serverside

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,3 +1,26 @@
-export default function profilePage() {
+import { InferGetServerSidePropsType, GetServerSideProps, GetServerSidePropsContext } from "next";
+import axios from "axios";
+
+export default function profilePage(props: GetServerSideProps) {
+  console.log("props: ", props);
   return <>profile</>;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const dev = process.env.NODE_ENV === "development";
+
+  if (dev) {
+    const res = await axios.get("http://localhost:3000/api/hello");
+    console.log("res: ", res);
+    return {
+      props: { data: res.data }, // will be passed to the page component as props
+    };
+  }
+
+  if (!dev) {
+    const res = await axios.get("http://52.79.36.122:3000/api/hello");
+    return {
+      props: { data: res.data }, // will be passed to the page component as props
+    };
+  }
 }
